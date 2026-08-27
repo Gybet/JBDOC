87dcb235be5f23f4ab92dc04839ce7d5812d3bd7
jbproust <jeanbaptiste.proust@gmail.com>
2026-08-27 22:15:38 +0200

    debut de doc sur argparse

---
+++ outils/Python/01-ArgParse/argparse_example.py
@@ -0,0 +1,31 @@
+import argparse
+
+#initialisation du parser (obligatoire)
+parser = argparse.ArgumentParser()
+
+
+#definition d'un argument positionnel
+parser.add_argument("argpos")
+
+#definition d'un argument optionnel
+parser.add_argument("--argopt")
+
+#conversion et val par defaut 
+parser.add_argument("--nbpote" , type = int , default = 0)
+
+#definition d un flag
+parser.add_argument("--surprise", action = "store_true")
+
+
+#lecture et affichage des arguments
+args = parser.parse_args()
+print(f"Bonjour, votre arg positionnel est  : {args.argpos}")
+if args.argopt != None :
+    print(f"Vous avez fourni l'argument optionnel : {args.argopt}")
+
+print(f"Vous avez {args.nbpote} amis ... c est pas beaucoup")
+
+if args.surprise == True :
+    print("SURPRISE VOUS AVEZ DECLENCHE LA FONCTION SURPRISE")
+
+
